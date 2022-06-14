cad42f40ff0b3cb09c98ff0a0f0d08092f78689a
kolbe zimmerman <kolbe.zimmerman-556@stu.mtec.edu>
2022-06-13 19:27:11 -0600

    contact

--- js/main.js
+++ js/main.js
@@ -67,3 +67,41 @@ const tabs = document.querySelectorAll('[data-target'),
             tab.classList.add('qualification__active')
         })
     })
+
+// SERVICES MODAL
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+// PORTFOLIO SWIPER
+let swiper = new Swiper('.mySwiper', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
